6f9126a51a055b35f93eb1e81498a000473b6ddc
Elvis Kerebi <kerebipreye@gmail.com>
2025-10-18 21:22:16 +0200

    connected welcome to splash

--- lib/features/splash/presentation/splash_page.dart
+++ lib/features/splash/presentation/splash_page.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 2), () {
+      if (!mounted) return;
+      Navigator.of(context).pushReplacementNamed('/welcome');
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
