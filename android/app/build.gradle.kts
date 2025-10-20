962cbbcd1fe7390587e934a25bcfa9f20218cc1b
falyseck <e.seck@alustudent.com>
2025-10-20 17:03:00 +0000

    debugged splash screen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,14 +20,12 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.kazi_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders["appAuthRedirectScheme"] = "com.googleusercontent.apps.943087778314-iqotm7kuon08kouuadtm0arapstisc7s"
     }
 
     buildTypes {
@@ -42,3 +40,5 @@ android {
 flutter {
     source = "../.."
 }
+
+android.experimentalProperties["android.ndk.suppressMinSdkVersionError"]=21
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import flutter_appauth
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
