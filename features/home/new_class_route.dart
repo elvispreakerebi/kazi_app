6038f260549b39057a8cb0e0ffd9c3e17366a759
falyseck <e.seck@alustudent.com>
2025-11-05 22:21:10 +0000

    new class route

---
+++ features/home/new_class_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'presentation/new_class_page.dart';
+
+class NewClassRoute {
+  static const String path = '/new-class';
+
+  static Route<dynamic> route(RouteSettings settings) {
+    return MaterialPageRoute(
+      settings: settings,
+      builder: (_) => const NewClassPage(),
+    );
+  }
+}
